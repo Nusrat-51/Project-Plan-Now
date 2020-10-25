f5d9bd2143113dd301683af70760c11d4489c9d9
Rafath Auvee <rafath.auvee@gmail.com>
2020-10-25 18:44:22 +0600

    25 October

--- lib/Admin/uploadItems.dart
+++ lib/Admin/uploadItems.dart
@@ -9,19 +9,101 @@ import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:image/image.dart' as ImD;
 
-
-class UploadPage extends StatefulWidget
-{
+class UploadPage extends StatefulWidget {
   @override
   _UploadPageState createState() => _UploadPageState();
 }
 
-class _UploadPageState extends State<UploadPage> with AutomaticKeepAliveClientMixin<UploadPage>
-{
+class _UploadPageState extends State<UploadPage>
+    with AutomaticKeepAliveClientMixin<UploadPage> {
   bool get wantKeepAlive => true;
 
   @override
   Widget build(BuildContext context) {
-    return Text("");
+    return displayAdminHomeScreen();
+  }
+
+  displayAdminHomeScreen() {
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: new BoxDecoration(
+            gradient: new LinearGradient(
+              colors: [Colors.green, Colors.lightGreenAccent],
+              begin: const FractionalOffset(0.0, 0.0),
+              end: const FractionalOffset(1.0, 0.0),
+              stops: [0.0, 1.0],
+              tileMode: TileMode.clamp,
+            ),
+          ),
+        ),
+        leading: IconButton(
+          icon: Icon(
+            Icons.border_color,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Route route = MaterialPageRoute(builder: (c) => AdminShiftOrders());
+            Navigator.pushReplacement(context, route);
+          },
+        ),
+        actions: [
+          FlatButton(
+            child: Text(
+              "Logout",
+              style: TextStyle(
+                color: Colors.pink,
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onPressed: () {
+              Route route = MaterialPageRoute(builder: (c) => SplashScreen());
+              Navigator.pushReplacement(context, route);
+            },
+          ),
+        ],
+      ),
+      body: getAdminHomeScreenBody(),
+    );
+  }
+
+  getAdminHomeScreenBody() {
+    return Container(
+      decoration: new BoxDecoration(
+        gradient: new LinearGradient(
+          colors: [Colors.green, Colors.lightGreenAccent],
+          begin: const FractionalOffset(0.0, 0.0),
+          end: const FractionalOffset(1.0, 0.0),
+          stops: [0.0, 1.0],
+          tileMode: TileMode.clamp,
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.shop_two,
+              color: Colors.white,
+              size: 200.0,
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20.0),
+              child: RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(9.0)),
+                child: Text(
+                  "Add New Items",
+                  style: TextStyle(fontSize: 20.0, color: Colors.white),
+                ),
+                color: Colors.green,
+                onPressed: () => print("Click"),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
